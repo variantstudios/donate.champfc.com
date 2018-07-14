8adb04dcfd62076a5585e6f87688523f01ae95f6
Brent Robbins <brent@variantstudios.com>
2018-07-14 17:19:33 -0600

    Stripe form tweaks to dynamically change the amount.

--- assets/js/dev/stripe.js
+++ assets/js/dev/stripe.js
@@ -34,4 +34,36 @@ if (queryString() == '200') {
 
 var a = $('input[name=donationOptions]:checked').val(); //getter
 
-$('.stripe-button').attr("data-amount",a); //setter
+$('.stripe-button').attr('data-amount',a); //setter
+
+var paramName = 'amount';
+var formAction = $('#stripeForm').attr('action');
+// $('#language li').click( function(e) {
+//   $('#languageLink').attr('href', function(i,a){
+//     var tmpRegex = new RegExp('(' + paramName + '=)[a-z]+', 'ig');
+//     return a.replace(tmpRegex, '$1'+e.target.dataset.language);
+//   });
+// });
+// $(formAction).attr('href', function(i,a){
+// 	var tmpRegex = new RegExp('(' + paramName + '=)[a-z]+', 'ig');
+// 	return a.replace(tmpRegex, '$1'+e.target.dataset.language);
+// });
+
+//$('#stripeForm').attr('action').set('amount', 42);
+
+
+var text = formAction;
+
+var newText = text.replace(/(amount=).*?(&|$)/,'$1' + a + '$2');
+
+$('#stripeForm').attr('action', newText);
+
+//console.log(newText);
+$( 'input[name=donationOptions]' ).click(function() {
+
+	var a = $('input[name=donationOptions]:checked').val(); 
+	var newText = text.replace(/(amount=).*?(&|$)/,'$1' + a + '$2');
+	$('#stripeForm').attr('action', newText);
+	$('.stripe-button').attr('data-amount',a);
+
+});
